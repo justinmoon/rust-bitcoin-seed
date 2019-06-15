91948f7bf26a16fd0d1df4a36fc8721f6ba79a36
Justin Moon <mail@justinmoon.com>
2019-06-15 18:13:25 -0500

    remove dead code

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,5 @@
-use bitcoin::network::constants::Network;
-use std::env;
-
 mod crawler;
-mod dns;
-mod echo;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args[1] == "serve" {
-        echo::serve();
-    } else if args[1] == "client" {
-        echo::client(args[2].as_bytes());
-    } else if args[1] == "dns" {
-        let seeds = dns::dns_seed(Network::Bitcoin);
-        println!("{:?}", seeds);
-    } else if args[1] == "crawl" {
-        crawler::crawl();
-    }
+    crawler::crawl();
 }
